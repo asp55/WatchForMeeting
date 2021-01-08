eef2a1d66e77e47c75a8678326a0fac51d3fc959
Andrew Parnell <aparnell@gmail.com>
2021-01-08 04:02:45 -0800

    Moved node server into its own repo, modified spoon to communicate to seperate server.

--- init.lua
+++ init.lua
@@ -1,41 +1,55 @@
 --- === WatchForMeeting ===
 ---
---- A module that monitors whether or not you're in a meeting, and updates a self served webpage
+--- A module that monitors whether or not you're in a meeting
 
-local obj={}
-obj.__index = obj
+-- create a namespace
+
+local WatchForMeeting={}
+WatchForMeeting.__index = WatchForMeeting
 
 -- Metadata
-obj.name = "WatchForMeeting"
-obj.version = "0.1"
-obj.author = "Andrew Parnell <aparnell@gmail.com>"
-obj.homepage = "https://github.com/asp55/WatchForMeeting"
-obj.license = "MIT - https://opensource.org/licenses/MIT"
+WatchForMeeting.name = "WatchForMeeting"
+WatchForMeeting.version = "0.1"
+WatchForMeeting.author = "Andrew Parnell <aparnell@gmail.com>"
+WatchForMeeting.homepage = "https://github.com/asp55/WatchForMeeting"
+WatchForMeeting.license = "MIT - https://opensource.org/licenses/MIT"
 
 --- WatchForMeeting.logger
 --- Variable
 --- Logger object used within the Spoon. Can be accessed to set the default log level for the messages coming from the Spoon.
-obj.logger = hs.logger.new('WatchMeeting')
+WatchForMeeting.logger = hs.logger.new('WatchMeeting')
 
 -------------------------------------------
 -- Declare Variables
 -------------------------------------------
 
-obj.port = 12345
-obj.meetingState = false
-obj.zoom = nil
+
+WatchForMeeting.sharing = {
+   selfHost=true,
+   port=12345
+}
+
+WatchForMeeting.running = false
+--Configuration Variables
+WatchForMeeting.room = hs.host.localizedName()
+WatchForMeeting.waitBeforeRetry = 0.5
+WatchForMeeting.maxConnectionAttempts = 3
+
+WatchForMeeting.meetingState = false
+WatchForMeeting.zoom = nil
 
 -------------------------------------------
---End of Declare Variables
+-- End of Declare Variables
 -------------------------------------------
+
 
 -------------------------------------------
 -- Web Server
 -------------------------------------------
-local server = hs.httpserver.new()
+local server = nil
 
 local function panelJSON() 
-   local message = {type="update", inZoom=obj.meetingState}
+   local message = {action="update", inZoom=WatchForMeeting.meetingState}
    return hs.json.encode(message)
 end
 
@@ -44,23 +58,14 @@ local htmlContent = monitorfile:read("*a")
 monitorfile:close()
 
 local function httpCallback()
-   local websocketPath = "ws://"..hs.network.interfaceDetails(hs.network.primaryInterfaces())["IPv4"]["Addresses"][1]..":"..obj.port.."/ws"
+   local websocketPath = "ws://"..hs.network.interfaceDetails(hs.network.primaryInterfaces())["IPv4"]["Addresses"][1]..":"..WatchForMeeting.sharing.port.."/ws"
    htmlContent = string.gsub(htmlContent,"%%websocketpath%%",websocketPath)
    return htmlContent, 200, {}
 end
 
 local function websocketCallback(msg)
-   if(msg == "Connected") then
-      obj.logger.d("Client connected: ",msg)
-      return panelJSON()
-   elseif(string.sub(msg, 0, 7) == "Update:") then
-      obj.logger.d("Update: ", string.sub(msg,7))
-   else
-      obj.logger.d("Message received: ",msg)
-      return "Received "..msg
-   end
+   return panelJSON()
 end
-
 -------------------------------------------
 -- End Web Server
 -------------------------------------------
@@ -70,7 +75,7 @@ end
 -------------------------------------------
 
 local function checkInMeeting()
-   return (obj.zoom ~= nil and obj.zoom:getMenuItems()[2].AXTitle == "Meeting")
+   return (WatchForMeeting.zoom ~= nil and WatchForMeeting.zoom:getMenuItems()[2].AXTitle == "Meeting")
 end
 
 --declare startStopWatchMeeting before watchMeeting, define it after.
@@ -84,34 +89,34 @@ local watchMeeting = hs.timer.new(0.5, function()
       return
     else 
       --Watch for zoom menu items
-      local _mic_open = obj.zoom:findMenuItem({"Meeting", "Unmute Audio"})==nil
-      local _video_on = obj.zoom:findMenuItem({"Meeting", "Start Video"})==nil
-      local _sharing = obj.zoom:findMenuItem({"Meeting", "Start Share"})==nil
-      if((obj.meetingState.mic_open ~= _mic_open) or (obj.meetingState.video_on ~= _video_on) or (obj.meetingState.sharing ~= _sharing)) then
-         obj.meetingState = {mic_open = _mic_open, video_on = _video_on, sharing = _sharing}
-         obj.logger.d("In Meeting: ", (obj.meetingState and true)," Open Mic: ",obj.meetingState.mic_open," Video-ing:",obj.meetingState.video_on," Sharing",obj.meetingState.sharing)
-         server:send(panelJSON())
+      local _mic_open = WatchForMeeting.zoom:findMenuItem({"Meeting", "Unmute Audio"})==nil
+      local _video_on = WatchForMeeting.zoom:findMenuItem({"Meeting", "Start Video"})==nil
+      local _sharing = WatchForMeeting.zoom:findMenuItem({"Meeting", "Start Share"})==nil
+      if((WatchForMeeting.meetingState.mic_open ~= _mic_open) or (WatchForMeeting.meetingState.video_on ~= _video_on) or (WatchForMeeting.meetingState.sharing ~= _sharing)) then
+         WatchForMeeting.meetingState = {mic_open = _mic_open, video_on = _video_on, sharing = _sharing}
+         WatchForMeeting.logger.d("In Meeting: ", (WatchForMeeting.meetingState and true)," Open Mic: ",WatchForMeeting.meetingState.mic_open," Video-ing:",WatchForMeeting.meetingState.video_on," Sharing",WatchForMeeting.meetingState.sharing)
+         if(server) then server:send(panelJSON()) end
       end
    end
 end)
 
 startStopWatchMeeting = function()
-   if(obj.meetingState == false and checkInMeeting() == true) then
-      obj.logger.d("Start Meeting")
-         obj.meetingState = {}
+   if(WatchForMeeting.meetingState == false and checkInMeeting() == true) then
+      WatchForMeeting.logger.d("Start Meeting")
+         WatchForMeeting.meetingState = {}
          watchMeeting:start()
          watchMeeting:fire()
-   elseif(obj.meetingState and checkInMeeting() == false) then
-      obj.logger.d("End Meeting")
+   elseif(WatchForMeeting.meetingState and checkInMeeting() == false) then
+      WatchForMeeting.logger.d("End Meeting")
       watchMeeting:stop()
-      obj.meetingState = false
-      server:send(panelJSON())
+      WatchForMeeting.meetingState = false
+      if(server) then server:send(panelJSON()) end
    end
 end
 
 local function checkMeetingStatus(window, name, event)
-	obj.logger.d("Check Meeting Status",window,name,event)
-   obj.zoom = window:application() --hs.application.find("zoom.us")
+	WatchForMeeting.logger.d("Check Meeting Status",window,name,event)
+   WatchForMeeting.zoom = window:application() --hs.application.find("zoom.us")
    
    startStopWatchMeeting()
 
@@ -130,18 +135,121 @@ zoomWindowFilter:pause()
 -- End of Zoom Monitor
 -------------------------------------------
 
-function obj:start()
-   server:websocket("/ws", websocketCallback)
-   server:setPort(self.port)
-   server:setCallback(httpCallback)
-   server:start()
-   zoomWindowFilter:resume()
+
+local _connectionAttempts = 0
+local _connectionError = false
+
+
+--Declare function before start connection because they're circular
+local function retryConnection()
+end
+local function stopConnection()
+   if(server) then
+      if(getmetatable(server).stop) then server:stop() end
+      if(getmetatable(server).close) then server:close() end
+   end
+end
+
+local function localWebsocketCallback(type, message)
+   if(type=="open") then
+      _connectionAttempts = 0
+
+      local draft = {action="identify", key=WatchForMeeting.sharing.key, name=WatchForMeeting.room, type="room", status={inZoom=WatchForMeeting.meetingState}} 
+      server:send(hs.json.encode(draft))
+   elseif(type == "closed" and WatchForMeeting.running) then
+      if(_connectionError) then
+         WatchForMeeting.logger.d("Lost connection to websocket, will not reattempt due to error")
+      else
+         WatchForMeeting.logger.d("Lost connection to websocket, attempting to reconnect in "..WatchForMeeting.waitBeforeRetry.." seconds")
+         retryConnection()
+      end
+   elseif(type == "fail") then
+      WatchForMeeting.logger.d("Could not connect to websocket server. attempting to reconnect in "..WatchForMeeting.waitBeforeRetry.." seconds. (Attempt ".._connectionAttempts.."/"..WatchForMeeting.maxConnectionAttempts..")")
+      retryConnection()
+   elseif(type == "received") then
+      local parsed = hs.json.decode(message);
+      if(parsed.error) then
+         _connectionError = true;
+         if(parsed.errorType == "badkey") then
+            stopConnection()
+            hs.showError("")
+            WatchForMeeting.logger.e("sharing.key not valid")
+         end
+      else
+         WatchForMeeting.logger.d("Websocket Message received: ", hs.inspect.inspect(parsed));
+      end
+
+   else
+      WatchForMeeting.logger.d("Websocket Callback "..type, message) 
+   end
+end
+
+
+local function startConnection() 
+   if(WatchForMeeting.sharing) then
+      if(WatchForMeeting.sharing.useServer) then
+         _connectionAttempts = _connectionAttempts + 1
+         server = hs.websocket.new(WatchForMeeting.sharing.url, localWebsocketCallback);
+      else
+         server = hs.httpserver.new()
+         server:websocket("/ws", websocketCallback)
+         server:setPort(WatchForMeeting.sharing.port)
+         server:setCallback(httpCallback)
+         server:start()
+      end
+   end
+end
+
+retryConnection = function()
+   if(_connectionAttempts >= WatchForMeeting.maxConnectionAttempts) then 
+      WatchForMeeting.logger.e("Maximum Connection Attempts failed")
+      stopConnection()
+   elseif(_connectionError) then
+      stopConnection()
+   else
+      hs.timer.doAfter(WatchForMeeting.waitBeforeRetry, startConnection) 
+   end
+end
+
+
+local function validateShareSettings(settings)
+   if(settings) then
+      if(settings.useServer and (settings.url==nil or settings.key==nil)) then
+         hs.showError("")
+         if(settings.url==nil) then WatchForMeeting.logger.e("sharing.url required when using a server") end
+         if(settings.key==nil) then WatchForMeeting.logger.e("sharing.key required when using a server") end
+         return false
+      elseif(not settings.useServer and settings.port==nil) then
+         hs.showError("")
+         WatchForMeeting.logger.e("sharing.port required when self hosting")
+         return false
+      else
+         return true
+      end
+   end
+   return false
+end
+
+
+function WatchForMeeting:start()
+   if(not self.running) then
+      self.running = true
+      if(validateShareSettings(self.sharing)) then
+         startConnection()
+      end
+      zoomWindowFilter:resume()
+   else
+      hs.showError("")
+      WatchForMeeting.logger.e("Cannot start, already running.")
+   end
    return self
 end
 
-function obj:stop()
-   server:stop()
+function WatchForMeeting:stop()
+   self.running = false
+   stopConnection()
    zoomWindowFilter:pause()
+   return self
 end
 
-return obj
+return WatchForMeeting
